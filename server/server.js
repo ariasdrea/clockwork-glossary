3fe3b14179cbd9c75d4777879fdb80c0c11bdb4d
Andrea Arias <ariasdrea@gmail.com>
2021-10-26 15:33:10 +0200

    setup

--- server/server.js
+++ server/server.js
@@ -12,5 +12,5 @@ app.get("*", function (req, res) {
 });
 
 app.listen(process.env.PORT || 3001, function () {
-    console.log("I'm listening.");
+    console.log("What's it going to be then, eh?");
 });
