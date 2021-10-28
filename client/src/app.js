804789732be47ee78009b1170cf45e7f6954b1e1
Andrea Arias <ariasdrea@gmail.com>
2021-10-28 18:10:34 +0200

    extracted autocomplete logic into its own component

--- client/src/app.js
+++ client/src/app.js
@@ -1,27 +1,10 @@
-import { useEffect, useState } from "react";
+import Autocomplete from "./autocomplete";
 
 export default function App() {
-    const [slovos, setSlovos] = useState([]);
-    useEffect(() => {}, []);
-
-    const grabInput = ({ target: { value } }) => {
-        if (value.length > 0) {
-            fetch(`/get-meaning/${value}`)
-                .then((res) => res.json())
-                .then((results) => setSlovos(results));
-        }
-    };
-
     return (
         <div>
-            <h1>Hello</h1>
-            <input type="text" onChange={(e) => grabInput(e)} />
-            {slovos &&
-                slovos.map((each, idx) => (
-                    <div key={idx}>
-                        <div>{each.slovo}</div>
-                    </div>
-                ))}
+            <h1>🕰</h1>
+            <Autocomplete />
         </div>
     );
 }
