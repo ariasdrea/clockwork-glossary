ebdfed3099ddc600a94a1c851ee5083264762519
Andrea Arias <ariasdrea@gmail.com>
2021-10-29 11:52:52 +0200

    adding inserts into heroku

--- server/addInitialData.js
+++ server/addInitialData.js
@@ -1,8 +1,10 @@
 const { addSlovos } = require("./db");
 const data = require("./data");
 
-for (let i = 0; i < data.length; i++) {
-    for (let prop in data[i]) {
-        addSlovos(prop, data[i][prop]);
+if (process.env.DATABASE_URL) {
+    for (let i = 0; i < data.length; i++) {
+        for (let prop in data[i]) {
+            addSlovos(prop, data[i][prop]);
+        }
     }
 }
